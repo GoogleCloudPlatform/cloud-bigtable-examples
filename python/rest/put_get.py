9d9dcba8c0a0757b010b9ccc3e3d2253615fbb8a
Bill Prin <waprin@google.com>
2015-04-14 13:28:12 -0700

    Fix some broken requests in put_get

--- python/rest/put_get.py
+++ python/rest/put_get.py
@@ -79,10 +79,10 @@ assert got_value == value
 
 
 # now we can delete the value
-requests.delete(base_url + "/" + table_name + row_key)
+requests.delete(base_url + "/" + table_name + "/" +row_key)
 
 # verify we now get a 404 when attempting to GET the value
-request = requests.get(base_url + "/" + table_name + row_key,
+request = requests.get(base_url + "/" + table_name + "/" + row_key,
                        headers={"Accept": "application/json"})
 print request.status_code
 assert request.status_code == 404
