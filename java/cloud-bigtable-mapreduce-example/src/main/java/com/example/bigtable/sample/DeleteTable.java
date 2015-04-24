f0911238975780ff4fa6c1065483bfef73d946be
Les Vogel <lesv@google.com>
2015-04-24 00:11:55 -0700

    One last set of renames...

--- java/cloud-bigtable-mapreduce-example/src/main/java/com/example/bigtable/sample/DeleteTable.java
+++ java/cloud-bigtable-mapreduce-example/src/main/java/com/example/bigtable/sample/DeleteTable.java
@@ -16,7 +16,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.google.bigtable.sample;
+package com.example.bigtable.sample;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
--- java/cloud-bigtable-mapreduce-example/src/main/java/com/example/bigtable/sample/SampleDriver.java
+++ java/cloud-bigtable-mapreduce-example/src/main/java/com/example/bigtable/sample/SampleDriver.java
@@ -16,7 +16,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.google.bigtable.sample;
+package com.example.bigtable.sample;
 
 import org.apache.hadoop.hbase.mapreduce.Export;
 import org.apache.hadoop.util.ProgramDriver;
