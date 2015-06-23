19327c309cbe578481e930e00c65a52741b1bdc6
Les Vogel <lesv@google.com>
2015-06-22 18:49:48 -0700

    Update to v 0.1.9 of Bigtable Artifact
    1. upgrade to 0.1.9 of the artifact & 1.1 of HBase
    2. mv hello world -> bigtable-helloworld
    3. fixes to readme
    4. Change all logging to use servlet container logging

--- java/managed-vms/bigtable-hello/src/main/java/com.example.cloud.bigtable.helloworld/OkServlet.java
+++ java/managed-vms/bigtable-hello/src/main/java/com.example.cloud.bigtable.helloworld/OkServlet.java
@@ -23,11 +23,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class OkServlet extends HttpServlet {
-  private static final Logger LOG = LoggerFactory.getLogger(OkServlet.class);
 
   public OkServlet() {
     super();
