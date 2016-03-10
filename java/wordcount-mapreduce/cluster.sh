b0e12aabc51dfbcf16e8e1fa1d5c0d1bff53cbf1
Les Vogel <lesv@google.com>
2016-03-10 13:09:59 -0800

    Update to 0.2.3 client
    also remove beta from Dataproc script.

--- java/wordcount-mapreduce/cluster.sh
+++ java/wordcount-mapreduce/cluster.sh
@@ -13,7 +13,7 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 
-PHASE="beta"
+PHASE=""
 DEFAULT_ZONE="us-central1-b"
 DEFAULT_CLUSTER='dp'
 
